908f7e2d143619e11d02411213c9c925a6d1abd2
jrekCody <jayrek_tabasa@cody.inc>
2024-12-01 13:43:13 +0800

    Initial Commit

---
+++ randomuser/randomuserApp.swift
@@ -0,0 +1,17 @@
+//
+//  randomuserApp.swift
+//  randomuser
+//
+//  Created by Jayrek Tabasa on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct randomuserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
